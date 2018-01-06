21307d30d9ec888a205fcb9016fcd37142ce0c91
Lê Xuân Trường <xuantruong.220494@gmail.com>
2018-01-06 17:34:18 +0700

    edit
    edit

---
+++ sosanhungvien.php
@@ -0,0 +1,82 @@
+<?php include('header.php') ?>
+
+<div class="col-md-12">
+	<h3><p class="tencongty"><a href="chitietcongviec.php">Quản đốc xưởng</a></p></h3>
+	<h4><p>Công ty TNHH Xuân Trường</p></h4>
+</div>
+<div class="col-md-12">
+	<div class="col-md-6">
+		<p><div class="well">
+			Đây là thông tin về hồ sơ JobStreet.com của bạn. Thông tin chưa chính xác? Cập nhật tại đây.
+		</div></p>
+		<table class="table">
+			<thead>
+				<tr>
+					<td>Chức danh</td>
+					<td>Giám đốc</td>
+
+				</tr>
+				<tr>
+					<td>Mức lương mong muốn</td>
+					<td>18 triệu đồng</td>
+
+				</tr>
+				<tr>
+					<td>Kinh nghiệm</td>
+					<td>20 năm</td>
+
+				</tr>
+				<tr>
+					<td>Trình độ học vấn</td>
+					<td>PGs.TS</td>
+
+				</tr>
+				<tr>
+					<td>Chuyên ngành</td>
+					<td>Cơ điện</td>
+
+				</tr>
+			</thead>
+
+		</table>
+		
+	</div>
+	<div class="col-md-6" style="border: 1px solid #dddd">
+		<p><div class="well">
+			So Sánh Với Các Ứng Viên Khác <br> Hệ thống tự thống kê
+		</div></p>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>So sánh</th>
+					<th>Học vấn</th>
+					<th>Mức lương</th>
+					<th>Kinh nghiệm</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Thấp hơn</td>
+					<td>1</td>
+					<td>1</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Giống nhau</td>
+					<td>2</td>
+					<td>3</td>
+					<td>2</td>
+				</tr>
+				<tr>
+					<td>Cao hơn</td>
+					<td>0</td>
+					<td>2</td>
+					<td>2</td>
+				</tr>
+			</tbody>
+
+		</table>
+	</div>
+	<div class="col-md-6"></div>
+</div>
+<?php include('footer.php') ?>
