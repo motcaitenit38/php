de76c9f0c696dcdd194f62b4e9d4afd46bb12062
Lê Xuân Trường <xuantruong.220494@gmail.com>
2018-01-06 21:50:54 +0700

    haha

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php include('header.php') ?>
 
-<div class="alert alert-success" role="alert">Hiện tại các giao diện đang được đưa dữ liệu mẫu để hiển thị</div>
+<div class="alert alert-success" role="alert">
+	<ul><li>Hiện tại các giao diện đang được đưa dữ liệu mẫu để hiển thị</li>
+		<li>Đây chỉ là giao diện mô tả các trang và luồng dữ liệu để nắm bắt yêu cầu bài toán</li>
+		<li>Những giao diện này sẽ được thay đổi theo thiết kế chuẩn, seo, dễ nhìn, gọn gàng</li>
+		<li>Mọi thông tin trên giao diện này chỉ là giao diện demo</li>
+	</ul>
+
+</div>
 <div class="alert alert-info" role="alert">Xem các giao diện qua việc thực hiện click vào menu bên trái ==> không click vào các nút hay link trên giao diện bên phải vì dữ liệu demo.</div>
 <div class="alert alert-warning" role="alert">Danh sách các giao diện được phân ra thành 2 nhóm người dùng là: Người tìm việc và Người đăng tuyển việc</div>
 <div class="alert alert-danger" role="alert">Vì một số yêu cầu chưa được rõ ràng nên chưa thực hiện chính xác theo yêu cầu.</div>
