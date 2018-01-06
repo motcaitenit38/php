bcc4e5b7b2dfc67657bf745c8528de21e5996951
Lê Xuân Trường <xuantruong.220494@gmail.com>
2018-01-06 23:20:13 +0700

    Revert "Revert "Merge branch 'phpgiaodien' of https://github.com/motcaitenit38/php into phpgiaodien""
    This reverts commit 32a3d971618526b74e6f65cfcea92e26319a8754.

---
+++ nhatuyedungxemsosanhungvien.php
@@ -0,0 +1,105 @@
+<?php include('header.php') ?>
+
+<div class="col-md-12">
+	<h3><p class="tencongty"><a href="chitietcongviec.php">Quản đốc xưởng</a></p></h3>
+	<h4><p>Công ty TNHH Xuân Trường</p></h4>
+</div>
+<div class="col-md-12"  style="border: 1px solid #dddd">
+	
+	
+	<p><div class="well">
+		Thống kê 
+	</div></p>
+	<table class="table">
+		<thead>
+			<tr>
+				<th>So sánh</th>
+				<th>Lĩnh vực hoạt động</th>
+				<th>năm kinh nghiệm</th>
+				<th>Vốn điều lệ</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Thấp hơn</td>
+				<td>1</td>
+				<td>1</td>
+				<td>1</td>
+			</tr>
+			<tr>
+				<td>Giống nhau</td>
+				<td>2</td>
+				<td>3</td>
+				<td>2</td>
+			</tr>
+			<tr>
+				<td>Cao hơn</td>
+				<td>0</td>
+				<td>2</td>
+				<td>2</td>
+			</tr>
+		</tbody>
+
+	</table>
+
+	<div class="col-md-6"></div>
+</div>
+<div class="col-md-12">
+	<div class="well">
+		Danh sách ứng viên khác cho vị trí này
+	</div>
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th>STT</th>
+				<th>Lĩnh Vực Hoạt Động</th>
+				<th>Giá chào </th>
+				<th>Năm KN </th>
+				<th>Vốn Điều lệ </th>
+				<th>Năng lực nhân sự </th>
+				<th>Năng lực thiết bị </th>
+				<th>Điểm đánh giá </th>
+				<th>Tác vụ</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>Xây Dựng Hạ Tầng</td>
+				<td>30 tỷ</td>
+				<td>6 </td>
+				<td>100 tỷ</td>
+				<td>-</td>
+				<td>-</td>
+				<td>7</td>
+				<td><button type="button" class="btn-success">Chọn thầu</button></td>
+			</tr>
+			<tr>
+
+				<td>1</td>
+				<td>Xây Dựng Công Trình</td>
+				<td>50 tỷ</td>
+				<td>4 </td>
+				<td>80 tỷ</td>
+				<td>-</td>
+				<td></td>
+				<td>5</td>
+				<td><button type="button" class="btn-success">Chọn thầu</button></td>
+
+			</tr>
+			<tr>
+
+				<td>1</td>
+				<td>Xây Dựng Công Trình Ngầm</td>
+				<td>20 tỷ</td>
+				<td>5 </td>
+				<td>120 tỷ</td>
+				<td>-</td>
+				<td></td>
+				<td>6</td>
+				<td><button type="button" class="btn-success">Chọn thầu</button></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+<?php include('footer.php') ?>
---
+++ sosanhungvien.php
@@ -0,0 +1,122 @@
+<?php include('header.php') ?>
+
+<div class="col-md-12">
+	<h3><p class="tencongty"><a href="chitietcongviec.php">Quản đốc xưởng</a></p></h3>
+	<h4><p>Công ty TNHH Xuân Trường</p></h4>
+</div>
+<div class="col-md-12">
+	<div class="col-md-6">
+		<p><div class="well">
+			Đây là thông tin về hồ sơ của bạn. Thông tin chưa chính xác? Cập nhật <a href="hosochitiet.php">tại đây</a>.
+		</div></p>
+		<table class="table">
+			<thead>
+				<tr>
+					<td>Lĩnh vực hoạt động</td>
+					<td>Xây dựng</td>
+
+				</tr>
+				<tr>
+					<td>Năm kinh nghiệm</td>
+					<td>18 năm</td>
+
+				</tr>
+				<tr>
+					<td>Vốn điều lệ</td>
+					<td>200 tỷ</td>
+
+				</tr>
+				<tr>
+					<td>Năng lực nhân sự</td>
+					<td>TS, Thạc sỹ</td>
+
+				</tr>
+				<tr>
+					<td>Năng lực thiết bị</td>
+					<td>Đầy đủ</td>
+
+				</tr>
+			</thead>
+
+		</table>
+		
+	</div>
+	<div class="col-md-6" style="border: 1px solid #dddd">
+		<p><div class="well">
+			So Sánh Với Các Ứng Viên Khác <br> Hệ thống tự thống kê
+		</div></p>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>So sánh</th>
+					<th>Lĩnh vực hoạt động</th>
+					<th>năm kinh nghiệm</th>
+					<th>Vốn điều lệ</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Thấp hơn</td>
+					<td>1</td>
+					<td>1</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Giống nhau</td>
+					<td>2</td>
+					<td>3</td>
+					<td>2</td>
+				</tr>
+				<tr>
+					<td>Cao hơn</td>
+					<td>0</td>
+					<td>2</td>
+					<td>2</td>
+				</tr>
+			</tbody>
+
+		</table>
+	</div>
+	<div class="col-md-6"></div>
+</div>
+<div class="col-md-12">
+	<div class="well">
+		3 ứng viên khác cho vị trí này
+	</div>
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Lĩnh vực</th>
+				<th>Năm kinh nghiệm</th>
+				<th>Vốn điều lệ</th>
+				<th>Năng lực thiết bị</th>
+				<th>Năng lực nhân sự</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Xây dựng</td>
+				<td>Cao hơn</td>
+				<td>Cao hơn</td>
+				<td>Thấp hơn</td>
+				<td>Cao hơn</td>
+			</tr>      
+			<tr class="success">
+				<td>Xây dựng</td>
+				<td>Cao hơn</td>
+				<td>Cao hơn</td>
+				<td>Thấp hơn</td>
+				<td>Cao hơn</td>
+			</tr>
+			<tr class="danger">
+				<td>Xây dựng</td>
+				<td>Cao hơn</td>
+				<td>Cao hơn</td>
+				<td>Thấp hơn</td>
+				<td>Cao hơn</td>
+			</tr>
+			
+		</tbody>
+	</table>
+</div>
+<?php include('footer.php') ?>
