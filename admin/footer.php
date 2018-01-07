eeaefcc0e8742166373d1234b09382128928ed28
Lê Xuân Trường <xuantruong.220494@gmail.com>
2018-01-07 12:01:31 +0700

    trang admin :))

---
+++ admin/footer.php
@@ -0,0 +1,20 @@
+</div>
+			<!--body wrapper start-->
+			</div>
+			 <!--body wrapper end-->
+		</div>
+        <!--footer section start-->
+			<footer>
+			   <p>&copy 2018 Admin Panel. All Rights Reserved | Design by <a href="#" target="_blank">Admin</a></p>
+			</footer>
+        <!--footer section end-->
+
+      <!-- main content end-->
+   </section>
+  
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/scripts.js"></script>
+<!-- Bootstrap Core JavaScript -->
+   <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
