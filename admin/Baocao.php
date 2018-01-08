82e777a0e7b357c9eb7cbfea88801e8ce4092315
thanhtung321 <35098145+thanhtung321@users.noreply.github.com>
2018-01-08 15:21:05 +0700

    Demo
    Demo

---
+++ admin/Baocao.php
@@ -0,0 +1,30 @@
+<?php include('header.php') ?>
+<div class="container">
+  <h2>Báo Cáo Theo Ngày</h2>
+  <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">Tin Tuyển Dụng</a></li>
+    <li><a data-toggle="tab" href="#menu1">Hồ Sơ Tuyển Dụng</a></li>
+    <li><a data-toggle="tab" href="#menu2">Hồ Sơ Tuyển Dụng Mới</a></li>
+    <li><a data-toggle="tab" href="#menu3">Hồ Sơ Tìm Việc</a></li>
+  </ul>
+
+  <div class="tab-content">
+    <div id="home" class="tab-pane fade in active">
+      <h3>Tin tuyển dụng</h3>
+      <p></p>
+    </div>
+    <div id="menu1" class="tab-pane fade">
+      <h3>Hồ sơ Tuyển Dụng</h3>
+      <p></p>
+    </div>
+    <div id="menu2" class="tab-pane fade">
+      <h3>Hồ Sơ Tuyển Dụng Mới</h3>
+      <p>Hồ sơ</p>
+    </div>
+    <div id="menu3" class="tab-pane fade">
+      <h3>Hồ Sơ Tìm Việc</h3>
+      <p></p>
+    </div>
+  </div>
+</div>
+		<?php include('footer.php') ?>
