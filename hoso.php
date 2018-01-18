96a4d2531fdb81c9a5abd1f1981e50c8b667bffe
thanhtung321 <35098145+thanhtung321@users.noreply.github.com>
2018-01-18 17:21:58 +0700

    sua
    sua

--- hoso.php
+++ hoso.php
@@ -16,7 +16,7 @@ include('headtimkiem.php'); ?>
     </div>
     <div class="col-sm-4">
       <div class="welll">
-       <p><a href="ProFile.html">Hồ Sơ Chi Tiết</a></p>
+       <p><a href="ProFile.html">My ProFile</a></p>
       </div>
       <div class="welll">
        <p>Đổi Mật Khẩu</p>
@@ -24,37 +24,29 @@ include('headtimkiem.php'); ?>
 	   <div class="welll">
             <p><a href="congviecdangyeucau.html">Công Việc Công ty Đã Đăng</a></p>
       </div>
+	   <div class="welll">
+            <p><a href="congviecquantam.php">Công Việc Quan Tâm</a></p>
+      </div>
     </div>
   </div>
 </div><br>
   
 <div class="text-center">    
-  <h3>Công việc đã quan tâm</h3><br>
+  <h3>Mô tả Công Ty</h3><br>
   <div class="row">
-    <div class="col-sm-4">
-        <div class="auto-style1">
-     <a href="danhgiatungcongviec.html"> + Tên Công việc: Cổng Thông Tin Điện Tử&nbsp;&nbsp;&nbsp;</a> <br />
-    + Tên Công ty đăng: Công ty CỔ Phần CPIT<br />
-    + Giá trị: <br />
-    + Địa điểm: Hà Nội<br />
-    + Tình trạng: Đã chào giá<br />
+  <div style="text-align: center; margin-left:80px">
+    <div class="col-sm-10" >
+        <div>
+    Công ty Cổ phần CPIT được thành lập từ Trung tâm Xây dựng và Phát triển công nghệ CPIT, năm 2005 Trung tâm chính thức đăng ký hoạt động. Khởi đầu của trung tâm là sự đóng góp, hợp sức từ 16 cán bộ tốt nghiệp chuyên ngành công nghệ thông tin đến từ các trường: Đại Học Bách Khoa Hà Nội, Đại Học Quốc Gia Hà Nội, Đại Học Kinh Tế Quốc Dân Hà Nội, Học viện Tài Chính…
+
+Với mục tiêu phát triển phần mềm đóng gói, đào tạo nguồn nhân lực công nghệ thông tin, gia công phần mềm ứng dụng và thương mại điện tử cho các đối tác trong và ngoài nước. Trung tâm CPIT liên tục đạt được kết quả tăng trưởng với tốc độ cao về số lượng nhân sự cũng như các dự án triển khai phần mềm.
+
+Sau gần 6 năm tích lũy kinh nghiệm từ hoạt động xây dựng và triển khai các giải pháp phần mềm, năm 2010 Trung tâm CPIT chính thức tham gia lĩnh vực thương mại phần mềm bằng việc tập trung và giới thiệu các hệ thống sản phẩm phần mềm đóng gói cho nhiều lĩnh vực ngành nghề khác nhau.
+
+Đến nay, Công ty Cổ phần CPIT đã đạt được những thành tựu to lớn trong công tác xây dựng và phát triển mạng lưới khách hàng và đội ngũ nhân sự cũng như đa dạng hóa hệ thống sản phẩm phần mềm của mình. CPIT đã xây dựng thành công số lượng lớn sản phẩm phần mềm đóng gói như một “giải pháp phần mềm tốt nhất” tới khách hàng trên toàn quốc từ sự đóng góp không ngừng của hơn 60 cán bộ lập trình, tư vấn, kinh doanh… có trình độ chuyên môn cao, tinh thần làm việc nhiệt tình, kỷ luật và năng động.
      </div>
     </div>
-    <div class="col-sm-4"> 
-        <div class="auto-style2">
-     <a href="danhgiatungcongviec.html"> + Tên Công việc: Cổng Thông Tin Điện Tử&nbsp;&nbsp;&nbsp;</a> <br />
-    + Tên Công ty đăng: Công ty CỔ Phần CPIT<br />
-    + Giá trị: <br />
-    + Địa điểm: Hà Nội<br />
-    + Tình trạng: Đã chào giá<br />
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="well">
-       <p><a href="congviecquantam.php">Xem Thêm</a></p>
-      </div>
-    
-    </div>
+   </div>
       
     
     </div>
